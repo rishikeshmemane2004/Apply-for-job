3938633b7dfe8c7694d9c563857c7d8f7f6a82ba
swapnil9096 <124584688+swapnil9096@users.noreply.github.com>
2023-03-25 19:21:21 +0530

    Add files via upload
    Adding new Files, Created utilities

--- Naukri/Applied_for_job.java
+++ Naukri/Applied_for_job.java
@@ -1,18 +1,11 @@
 package Naukri;
 
-import static org.openqa.selenium.support.locators.RelativeLocator.with;
-
-import java.io.FileWriter;
 import java.io.IOException;
 import java.time.Duration;
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Random;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
@@ -21,17 +14,15 @@ import org.testng.annotations.Test;
 
 public class Applied_for_job {
 	
-	Applied_for_job() throws IOException
-	{
-		write = new FileWriter("D:/ECLIPSE/My_Project/src/test/java/Naukri/Applied_Jobs"+ date +".txt");
-	}
 	
 	static WebDriver driver;
 	static int no;
-	FileWriter write;
+	
 	// creating random time for heating user action
 	static Random random = new Random();
 	static LocalDate date = LocalDate.now();
+	
+	
 	@BeforeMethod
 	public void setUp() 
 	{
@@ -63,79 +54,26 @@ public class Applied_for_job {
 		System.out.println(jobName);
 
 		// click on search jobs
-		driver.findElement(By.xpath("//div[@class='nI-gNb-sb__main']")).click();
-		no = random.nextInt(1000, Utilities.getTime());
-		Thread.sleep(no);
+		Utilities.clickOnSearchJob();
 
 		// entering job name
-		driver.switchTo().defaultContent();
-		driver.findElement(By.xpath("(//input[@type='text'])[1]")).sendKeys(jobName);
-		no = random.nextInt(1000, Utilities.getTime());
-		Thread.sleep(no);
+		Utilities.enterJobName(jobName);
 
 		// selecting experience
-		driver.findElement(By.id("experienceDD")).click();
-		no = random.nextInt(1000, Utilities.getTime());
-		Thread.sleep(no);
+		Utilities.selectExperience();
 
-		driver.findElement(By.cssSelector("li[title='Fresher']")).click();
-		no = random.nextInt(1000, Utilities.getTime());
-		Thread.sleep(no);
+		//click on Fresher
+		Utilities.clickOnFresher();
 
 		// entering location
-		WebElement loc = driver.findElement(By.cssSelector("input[placeholder='Enter location']"));
-		loc.sendKeys("Pune");
-		no = random.nextInt(1000, Utilities.getTime());
-		Thread.sleep(no);
+		Utilities.enterLocation("Mumbai");
 
 		// click on search button
-		driver.findElement(with(By.tagName("span")).toRightOf(loc)).click();
-		no = random.nextInt(1000, Utilities.getTime());
-		Thread.sleep(no);
+		Utilities.clickOnSearchButton();
 
 		// Applying for jobs
-		List<WebElement> low = driver.findElements(By.cssSelector("article[class='jobTuple']"));
-
-		for (int i = 0; i < low.size(); i++) {
-			low = driver.findElements(By.cssSelector("article[class='jobTuple']"));
-
-			low.get(i).click();
-			no = random.nextInt(1000, Utilities.getTime());
-			Thread.sleep(no);
-
-			driver.switchTo().window(Utilities.getChildWindows());
-
-			try {
-				if (driver.findElement(By.cssSelector(
-						"div[class='apply-button-container'] button[class='waves-effect waves-ripple btn apply-button']"))
-						.getText().contains("APPLY"))
-				{
-					
-					driver.findElement(By.xpath("(//button[@class='waves-effect waves-ripple btn apply-button'])[1]")).click();
-					no = random.nextInt(1000, Utilities.getTime());
-					Thread.sleep(no);
-
-					String appliedJob = driver.findElement(By.xpath("//span[@class='apply-message']//strong")).getText();
-					System.out.println(appliedJob);
-					
-					
-					write.write(appliedJob + Keys.ENTER);
-					
-					driver.switchTo().window(Utilities.getChildWindows()).close();
-					no = random.nextInt(1000, Utilities.getTime());
-					Thread.sleep(no);
-					driver.switchTo().window(Utilities.getParentWindows());
-				}
-			} catch (Exception e) 
-			{
-				System.out.println("Should done manualy only...!!");
-				driver.switchTo().window(Utilities.getChildWindows()).close();
-				no = random.nextInt(1000, Utilities.getTime());
-				Thread.sleep(no);
-				driver.switchTo().window(Utilities.getParentWindows());
-			}
-		}
-		write.close();
+		Utilities.Applying();
+		
 	}
 }
 
--- Naukri/Utilities.java
+++ Naukri/Utilities.java
@@ -1,17 +1,24 @@
 package Naukri;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 
 public class Utilities extends Applied_for_job{
 
-
-	Utilities() throws IOException {
+	static WebElement loc;
+	static FileWriter write;
+	
+	Utilities() throws IOException 
+	{
 		super();
-		// TODO Auto-generated constructor stub
+		write = new FileWriter("D:/ECLIPSE/My_Project/src/test/java/Naukri/Applied_Jobs"+ date +".txt");
 	}
 
 	public static String getParentWindows() {
@@ -59,5 +66,105 @@ public class Utilities extends Applied_for_job{
 		return no;
 	}
 
+	public static void sleep(int no) throws InterruptedException
+	{
+		Thread.sleep(no);
+	}
+	
+	public static void clickOnSearchJob() throws InterruptedException
+	{
+		driver.findElement(By.xpath("(//div[@class='nI-gNb-sb__main'])[1]")).click();
+		no = random.nextInt(1000, Utilities.getTime());
+		Utilities.sleep(no);
+	}
+	
+	public static void enterJobName(String jobName) throws InterruptedException
+	{
+		driver.switchTo().defaultContent();
+		driver.findElement(By.xpath("(//input[@type='text'])[1]")).sendKeys(jobName);
+		no = random.nextInt(1000, Utilities.getTime());
+		Utilities.sleep(no);
+	}
+	
+	public static void selectExperience() throws InterruptedException
+	{
+		driver.findElement(By.id("experienceDD")).click();
+		no = random.nextInt(1000, Utilities.getTime());
+		Utilities.sleep(no);
+	}
+	
+	public static void clickOnFresher() throws InterruptedException
+	{
+		driver.findElement(By.cssSelector("li[title='Fresher']")).click();
+		no = random.nextInt(1000, Utilities.getTime());
+		Utilities.sleep(no);
+	}
+	
+	public static void enterLocation(String location) throws InterruptedException
+	{
+		WebElement loc = driver.findElement(By.cssSelector("input[placeholder='Enter location']"));
+		loc.sendKeys(location);
+		no = random.nextInt(1000, Utilities.getTime());
+		Utilities.sleep(no);
+	}
+	
+	public static void clickOnSearchButton() throws InterruptedException
+	{
+		driver.findElement(By.xpath("//span[normalize-space()='Search']")).click();
+		no = random.nextInt(1000, Utilities.getTime());
+		Utilities.sleep(no);
+	}
+	
+	public static void Applying() throws InterruptedException, IOException
+	{
+		List<WebElement> low = driver.findElements(By.cssSelector("article[class='jobTuple']"));
+
+		for (int i = 0; i < low.size(); i++) {
+			low = driver.findElements(By.cssSelector("article[class='jobTuple']"));
+
+			low.get(i).click();
+			no = random.nextInt(1000, Utilities.getTime());
+			Utilities.sleep(no);
+
+			driver.switchTo().window(Utilities.getChildWindows());
+
+			try {
+				if (driver.findElement(By.cssSelector(
+						"div[class='apply-button-container'] button[class='waves-effect waves-ripple btn apply-button']"))
+						.getText().contains("APPLY"))
+				{
+					
+					driver.findElement(By.xpath("(//button[@class='waves-effect waves-ripple btn apply-button'])[1]")).click();
+					no = random.nextInt(1000, Utilities.getTime());
+					Utilities.sleep(no);
+
+					String appliedJob = driver.findElement(By.xpath("//span[@class='apply-message']//strong")).getText();
+					System.out.println(appliedJob);
+					
+					
+					write.write(appliedJob + Keys.ENTER);
+					
+					driver.switchTo().window(Utilities.getChildWindows()).close();
+					no = random.nextInt(1000, Utilities.getTime());
+					Utilities.sleep(no);
+					driver.switchTo().window(Utilities.getParentWindows());
+				}
+			} catch (Exception e) 
+			{
+				System.out.println("Should done manualy only...!!");
+				driver.switchTo().window(Utilities.getChildWindows()).close();
+				no = random.nextInt(1000, Utilities.getTime());
+				Utilities.sleep(no);
+				driver.switchTo().window(Utilities.getParentWindows());
+			}
+		}
+		write.close();
+	}
+	
+	
+	
+	
+	
+	
 
 }
